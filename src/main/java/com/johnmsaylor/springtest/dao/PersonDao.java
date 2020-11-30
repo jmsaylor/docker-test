3328438e7a493a0c3ad23481e81871dfe3e44f5d
jmsaylor <johnmsaylor@gmail.com>
2020-11-30 10:35:17 -0500

    added Dockerfile to Spring tutorial but cant map ports correctly yet

---
+++ src/main/java/com/johnmsaylor/springtest/dao/PersonDao.java
@@ -0,0 +1,17 @@
+package com.johnmsaylor.springtest.dao;
+
+import com.johnmsaylor.springtest.model.Person;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> getAllPeople();
+}
